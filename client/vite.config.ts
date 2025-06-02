06a6f7da479671757411b82628731957623e3016
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-02 17:48:29 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: '.',                      // this can be left if using monorepo
+  publicDir: 'public',            // makes Vite look here for index.html
+  build: {
+    outDir: 'dist',
+  }
 })
