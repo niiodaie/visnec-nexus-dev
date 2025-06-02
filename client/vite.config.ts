39c893d657e7e532a21c9306dbab63a06f5b0d6e
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-02 18:06:58 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // optional since you're already in client/
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   }
 })
