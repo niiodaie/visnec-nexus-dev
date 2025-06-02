b05dceed10dbbd71529f1b9c5cf36a1efc954e1e
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-02 17:58:01 -0500

    Add files via upload

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,8 +1,10 @@
-
 import type { Config } from 'tailwindcss'
+
 const config: Config = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
 export default config
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',                      // this can be left if using monorepo
-  publicDir: 'public',            // makes Vite look here for index.html
   build: {
     outDir: 'dist',
   }
