b7cfb0ed0787b20fda11a245ffae03924e76bb26
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-02 19:09:23 -0500

    Add files via upload

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,10 +1,8 @@
-import type { Config } from 'tailwindcss'
 
-const config: Config = {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-export default config
