a8311ca19b643a1da803e6c00606211ecaa7d79a
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-02 23:18:24 -0500

    Add files via upload

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+
 export default {
-  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
